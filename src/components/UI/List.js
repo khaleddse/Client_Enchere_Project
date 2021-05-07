e564a606b665fc627a1716fc7f79dc649e52c713
khaledse <selmikhaled25@gmail.com>
2021-05-07 04:01:45 +0200

    add search methode to list é delte warining

--- src/components/UI/List.js
+++ src/components/UI/List.js
@@ -1,58 +1,111 @@
-import {Table,Thead,Tbody, Tfoot, Tr, Th, Td, TableCaption, IconButton} from "@chakra-ui/react";
-import {DeleteIcon} from '@chakra-ui/icons'
-const List=({list,delte})=>{
-  return(
-<Table variant="striped" colorScheme="teal">
-  <TableCaption>Imperial to metric conversion factors</TableCaption>
-  <Thead>
-    <Tr>
-      <Th>User_Id</Th>
-      <Th>First_Name</Th>
-      <Th >Last_Name</Th>
-      <Th>E-mail_Adress</Th>
-      <Th isNumeric>Phone_Number</Th>
-      <Th isNumeric>Point</Th>
-      <Th>modifier</Th>
-      
-    </Tr>
-  </Thead>
-  <Tbody>
-  {list.map(({id,firstName,lastName,email,phone,point})=>{
-    return(<Tr>
-      <Td>{id}</Td>
-      <Td>{firstName}</Td>
-      <Td>{lastName}</Td>
-      <Td>{email}</Td>
-      <Td>{phone}</Td>
-      <Td>{point}</Td>
-      <Td>
-      <IconButton
-  variant="outline"
-  //colorScheme="teal"
-  aria-label="Send email"
-  bg="transparent"
-  icon={<DeleteIcon />}
-  onClick={()=>delte(id)}
-/>
-      </Td>
-    
-    </Tr>)
-  })}
-    
-    
-  </Tbody>
-  <Tfoot>
-    <Tr>
-      <Th>
-  
-</Th>
-      <Th>into</Th>
-      <Th isNumeric>multiply by</Th>
-      
-    </Tr>
-  </Tfoot>
-  
-</Table>
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
+import { DeleteIcon, SearchIcon } from "@chakra-ui/icons";
+import React, { useState } from "react";
+
+const List = ({ list, delte }) => {
+  const [text, setText] = useState({ search: "" });
+  const FilterChangeHandler = async (value) => {
+    if (value.trim().length > 0) setIsFiltred(true);
+    else setIsFiltred(false);
+
+    setText({ search: value });
+  };
+  const regex = new RegExp(text.search, "i");
+
+  const [isfiltred, setIsFiltred] = useState(false);
+
+  return (
+    <Table variant="striped" colorScheme="teal">
+      <TableCaption>Imperial to metric conversion factors</TableCaption>
+      <Thead>
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            children={<SearchIcon color="gray.300" />}
+          />
+          <Input
+            variant="flushed"
+            type="text"
+            placeholder="search User with E-mail_Adress,User_Id"
+            onChange={(e) => FilterChangeHandler(e.target.value)}
+          />
+        </InputGroup>
+      </Thead>
+      <Thead>
+        <Tr>
+          <Th>User_Id</Th>
+          <Th>First_Name</Th>
+          <Th>Last_Name</Th>
+          <Th>E-mail_Adress</Th>
+          <Th>Phone_Number</Th>
+          <Th>Point</Th>
+          <Th> modifier</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {isfiltred
+          ? list
+              .filter((user) => {
+                if (regex.test(user.email) || regex.test(user._id)) {
+                  return user;
+                } else {
+                  return null;
+                }
+              })
+              .map(({ id, firstName, lastName, email, phone, point }) => (
+                <Tr>
+                  <Td>{id}</Td>
+                  <Td>{firstName}</Td>
+                  <Td>{lastName}</Td>
+                  <Td>{email}</Td>
+                  <Td>{phone}</Td>
+                  <Td>{point}</Td>
+                  <Td>
+                    <IconButton
+                      variant="outline"
+                      //colorScheme="teal"
+                      aria-label="delte user"
+                      bg="transparent"
+                      icon={<DeleteIcon />}
+                      onClick={() => delte(id)}
+                    />
+                  </Td>
+                </Tr>
+              ))
+          : list.map(({ id, firstName, lastName, email, phone, point }) => (
+              <Tr>
+                <Td>{id}</Td>
+                <Td>{firstName}</Td>
+                <Td>{lastName}</Td>
+                <Td>{email}</Td>
+                <Td>{phone}</Td>
+                <Td>{point}</Td>
+                <Td>
+                  <IconButton
+                    variant="outline"
+                    //colorScheme="teal"
+                    aria-label="Send email"
+                    bg="transparent"
+                    icon={<DeleteIcon />}
+                    onClick={() => delte(id)}
+                  />
+                </Td>
+              </Tr>
+            ))}
+      </Tbody>
+    </Table>
   );
-}
-export default List
+};
+export default List;
